1d73a5c3d8f8376e3e10fe04ae4335bbf7ec65a4
Chodisetti Eswar Ramanjaneyulu <105968530+Eswarramanjaneyulu@users.noreply.github.com>
2025-07-06 05:51:04 +0000

    Classwork

---
+++ Classwork/6.tuple.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+
+
+
+#Tuple
+
+# Empty Tuple
+empty_tuple = ()
+# Single-element Tuple (note the trailing comma)
+single_tuple = (5,)
+# Multi-element Tuple
+my_tuple = (1, "apple", 3.5)
+# Without parentheses (implicit tuple creation)
+implicit_tuple = 1, 2, 3
