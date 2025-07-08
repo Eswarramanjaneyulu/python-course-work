87d97fe30f88e16982067adc8eade93f68bdd857
Chodisetti Eswar Ramanjaneyulu <105968530+Eswarramanjaneyulu@users.noreply.github.com>
2025-07-08 04:06:21 +0000

    Practicework

---
+++ Practicework/condition statements.py
@@ -0,0 +1,35 @@
+#1.positive or negative
+a=int(input("Enter a number : "))
+if a>0:
+    print("positive number")
+else:
+    print("negative number")
+ 
+ #even or odd
+a=int(input("Enter a number :  "))   
+if a % 2 ==0:
+     print("even")
+else:
+     print("odd")
+     
+#Divisible by 5
+a=int(input("Enter a number : "))
+if a%5==0:
+     print("Divisible by 5")
+else:
+     print("not divisable by 5")
+     
+#divisable by 3and 7
+a=int(input("Esnter a number : "))
+if a%3==0 and a%7==0:
+     print("Divisible by both 3 and 7")
+elif a%3==0 :
+     print("Divisible by  3 ")
+elif a%7==0 :
+     print("Divisible by 7")
+else:
+     print("none")
+     
+#check for leap year
+a=int(input("Enter a number : "))
+     
