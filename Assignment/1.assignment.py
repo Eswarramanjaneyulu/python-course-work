2a9ee4312537e9c27a3dfc3885631c9bd6f50354
Chodisetti Eswar Ramanjaneyulu <105968530+Eswarramanjaneyulu@users.noreply.github.com>
2025-07-11 12:01:12 +0000

    Assignment

--- Assignment/1.assignment.py
+++ Assignment/1.assignment.py
@@ -1,22 +1,22 @@
-data= {
-        101: {"name": "Mobile", "price": 15000,"Categories":"Electronics","Available Stock":100,"Sold Stock":20,"Discount Percentage":"2%","Product Features":"waterproof"},
-        102: {"name": "Laptop", "price": 50000,"Categories":"Electronics","Available Stock":70,"Sold Stock":5,"Discount Percentage":"5%","Product Features":"waterproof"},
-        103: {"name": "Headphones", "price": 1500,"Categories":"Electronics","Available Stock":200,"Sold Stock":10,"Discount Percentage":"2%","Product Features":"Bluetooth"},
-        104: {"name": "Shoes", "price": 2500,"Categories":"fitness","Available Stock":50,"Sold Stock":15,"Discount Percentage":"2.5%","Product Features":"Formal"},
-        105: {"name": "SmartWatch", "price": 3000,"Categories":"Electronics","Available Stock":100,"Sold Stock":50,"Discount Percentage":"3%","Product Features":"waterproof"}
-    }
-        
-a = int(input("Enter a Product ID: "))
-if a in data:
-    print("\nProduct Details:")
-    print("ID:", a)
-    print("Name:", data[a]["name"])
-    print("Price: ₹", data[a]["price"])
-    print("Category:", data[a]["Categories"])
-    print("Available Stock:", data[a]["Available Stock"])
-    print("Sold Stock:", data[a]["Sold Stock"])
-    print("Discount:", data[a]["Discount Percentage"])
-    print("Features:", data[a]["Product Features"])
-else:
-    print("Invalid Product ID. Please try again.")
+class Mini_Flipkart:
+    
+ product_id = int(input("Enter Product ID: "))
+ product_name = input("Enter Product Name: ")
+ product_price = float(input("Enter Product Price: "))
+ product_category = input("Enter Product Category: ")
+ available_stock = int(input("Enter Available Stock: "))
+ sold_stock = int(input("Enter Sold Stock: "))
+ discount_percentage = input("Enter Discount Percentage: ")
+ product_features = input("Enter Product Features: ")
+
+ print("\nProduct Details:")
+ print("ID:", product_id)
+ print("Name:", product_name)
+ print("Price: ₹", product_price)
+ print("Category:", product_category)
+ print("Available Stock:", available_stock)
+ print("Sold Stock:", sold_stock)
+ print("Discount:", discount_percentage)
+ print("Features:", product_features)
+
 
