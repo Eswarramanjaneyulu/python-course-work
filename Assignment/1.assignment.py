aa11e750b1f6a3db8f7d414e10473cbe28ebb33d
Chodisetti Eswar Ramanjaneyulu <105968530+Eswarramanjaneyulu@users.noreply.github.com>
2025-07-11 06:26:39 +0000

    Assignment

--- Assignment/1.assignment.py
+++ Assignment/1.assignment.py
@@ -1,8 +1,8 @@
 data= {
         101: {"name": "Mobile", "price": 15000,"Categories":"Electronics","Available Stock":100,"Sold Stock":20,"Discount Percentage":"2%","Product Features":"waterproof"},
         102: {"name": "Laptop", "price": 50000,"Categories":"Electronics","Available Stock":70,"Sold Stock":5,"Discount Percentage":"5%","Product Features":"waterproof"},
-        103: {"name": "Headphones", "price": 1500,"Categories":"Electronics","Available Stock":200,"Sold Stock":10,"Discount Percentage":"2%","Product Features":"waterproof"},
-        104: {"name": "Shoes", "price": 2500,"Categories":"fitness","Available Stock":50,"Sold Stock":15,"Discount Percentage":"2.5%","Product Features":"waterproof"},
+        103: {"name": "Headphones", "price": 1500,"Categories":"Electronics","Available Stock":200,"Sold Stock":10,"Discount Percentage":"2%","Product Features":"Bluetooth"},
+        104: {"name": "Shoes", "price": 2500,"Categories":"fitness","Available Stock":50,"Sold Stock":15,"Discount Percentage":"2.5%","Product Features":"Formal"},
         105: {"name": "SmartWatch", "price": 3000,"Categories":"Electronics","Available Stock":100,"Sold Stock":50,"Discount Percentage":"3%","Product Features":"waterproof"}
     }
         
